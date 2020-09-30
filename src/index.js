8dedc0ba92b9c1f23bf887867b634e20cf70643e
Admin <riverflow1928@yandex.ru>
2020-09-30 21:06:18 +0500

    feat: solve all typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return array !== undefined && array.length > 0 ? array.sort((a, b) => a > b ? 1 : a < b ? -1 : 0)[0] : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return array !== undefined && array.length > 0 ? array.sort((a, b) => a > b ? -1 : a < b ? 1 : 0)[0] : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  return array !== undefined && array.length > 0 ? array.reduce((el, acc) => el + acc, 0) / array.length : 0;
 }
